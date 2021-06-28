25e0d1f7db04bbb31013d292f0ad20c2a3fb5e41
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-28 23:52:40 +0800

    feat: Register routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,33 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Root from "../views/Root.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Root",
+    component: Root,
+    // beforeEnter: (to, from, next) => {
+    //   if (true) {
+
+    //   } else {
+
+    //   }
+    // }
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
   },
   {
     path: "/about",

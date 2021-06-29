83543d95798a2846fff6b7a774b2d32fdebcb5c9
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-29 15:55:29 +0800

    feat: Added empty topbar

---
+++ src/components/Topbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="topbar">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
+<style lang="sass" scoped>
+.topbar
+  position: sticky
+  top: 0
+  left: 0
+  width: 100%
+  height: 53px
+  padding: 0 16px
+  border-bottom: 1px solid #eee
+  background: white
+</style>
---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="user-layout">
+    <Topbar />
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Topbar from "../components/Topbar.vue";
+
+export default Vue.extend({
+  components: {
+    Topbar,
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+.user-layout
+  position: relative
+</style>

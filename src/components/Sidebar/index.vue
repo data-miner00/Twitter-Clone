042a9f067a77e871761634a2ff60956fd63e41a9
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-02 01:46:55 +0800

    feat: New header nav

--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <header class="header">
-    <nav class="header__sidebar">
-      <Item />
-    </nav>
-  </header>
+  <div class="sidebar">
+    <div class="sidebar__top">
+      <div class="sidebar__top__logo">
+        <img src="logo.svg" alt="logo" height="42" />
+      </div>
+      <div class="sidebar__top__links">
+        <Item
+          v-for="navLink in navLinks"
+          :key="navLink.link"
+          :linkTitle="navLink.link"
+          :icon="navLink.icon"
+          :to="navLink.to"
+        />
+      </div>
+      <div class="sidebar__top__tweet"></div>
+    </div>
+    <div class="sidebar__bottom"></div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,16 +27,66 @@ export default Vue.extend({
   components: {
     Item,
   },
+  computed: {
+    navLinks() {
+      return [
+        {
+          link: "Home",
+          icon: "igloo",
+          to: "/s",
+        },
+        {
+          link: "Explore",
+          icon: "hashtag",
+          to: "/s",
+        },
+        {
+          link: "Notifications",
+          icon: "bell",
+          to: "/s",
+        },
+        {
+          link: "Messages",
+          icon: "envelope",
+          to: "/s",
+        },
+        {
+          link: "Bookmarks",
+          icon: "bookmark",
+          to: "/s",
+        },
+        {
+          link: "Lists",
+          icon: "sticky-note",
+          to: "/s",
+        },
+        {
+          link: "Profile",
+          icon: "user",
+          to: "/s",
+        },
+        {
+          link: "More",
+          icon: "ellipsis-h",
+          to: "/s",
+        },
+      ];
+    },
+  },
 });
 </script>
 
 <style lang="sass" scoped>
-.header
-  display: flex
-  flex-grow: 1
-  justify-content: flex-end
-  &__sidebar
-    padding: 0 10px
-    max-width: 275px
-    min-width: 88px
+
+.sidebar
+  padding: 0 12px
+  width: 275px
+  // min-width: 88px
+  &__top
+    &__logo
+      display: flex
+      height: 48px
+      width: 48px
+      justify-content: center
+      align-items: center
 </style>

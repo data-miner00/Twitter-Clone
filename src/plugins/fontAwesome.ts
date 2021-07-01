71622227b2f3086c4d59ceaccaa68021e476f7ce
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-02 01:44:26 +0800

    feat: Added necessary fa icon

--- src/plugins/fontAwesome.ts
+++ src/plugins/fontAwesome.ts
@@ -3,13 +3,26 @@ import {
   faCoffee,
   faComment,
   faEllipsisH,
+  faHashtag,
   faHeart,
+  faHome,
+  faIgloo,
   faRetweet,
   faShare,
+  faBell,
+  faEnvelope,
+  faBookmark,
+  faStickyNote,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faComment as farComment,
   faHeart as farHeart,
+  faBell as farBell,
+  faEnvelope as farEnvelope,
+  faBookmark as farBookmark,
+  faStickyNote as farStickyNote,
+  faUser as farUser,
 } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import Vue from "vue";
@@ -22,7 +35,20 @@ library.add(
   faHeart,
   faShare,
   farComment,
-  farHeart
+  farHeart,
+  faHome,
+  faIgloo,
+  faHashtag,
+  faBell,
+  farBell,
+  faEnvelope,
+  farEnvelope,
+  faBookmark,
+  farBookmark,
+  faStickyNote,
+  farStickyNote,
+  faUser,
+  farUser
 );
 
 Vue.component("Fa", FontAwesomeIcon);

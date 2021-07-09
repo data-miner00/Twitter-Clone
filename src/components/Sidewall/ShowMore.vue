da7d772fe47356171b9a59581ea0c3ab3c24dd32
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-09 15:43:00 +0800

    feat: Added show more component

---
+++ src/components/Sidewall/ShowMore.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="show-more">Show more</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    //
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+.show-more
+  padding: 16px
+  color: rgb(121, 75, 196)
+  font-size: 15px
+  transition: background .2s
+  cursor: pointer
+  &:hover
+    background: rgba(0, 0, 0, 0.03)
+</style>

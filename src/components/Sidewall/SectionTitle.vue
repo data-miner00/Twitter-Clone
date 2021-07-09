af7f098eec36c9bb9d1c5f7e0c4e41d111f73274
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-09 13:28:50 +0800

    feat: Added section title component

---
+++ src/components/Sidewall/SectionTitle.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="section-title">
+    <div class="section-title__title">
+      <span>{{ text }}</span>
+    </div>
+    <div class="section-title__action-icon">
+      <div class="section-title__semi-wrap">
+        <fa :icon="['far', 'sun']" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    text: String,
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+.section-title
+  padding: 12px 16px
+  display: flex
+  border-bottom: 1px solid rgb(239, 243, 244)
+  align-items: center
+  &__title
+    font-size: 20px
+    font-weight: 800
+    flex-grow: 1
+
+  &__action-icon
+    height: 20px
+
+  &__semi-wrap
+    min-height: 32px
+    font-size: 20px
+    min-width: 32px
+    align-self: stretch
+    justify-content: center
+    display: flex
+    align-items: center
+    padding: 8px
+    transition: background .2s
+    border-radius: 250px
+    color: rgb(121, 75, 196)
+    cursor: pointer
+    transform: translateY(-8px)
+
+    &:hover
+      background: rgba(121, 75, 196, .1)
+</style>

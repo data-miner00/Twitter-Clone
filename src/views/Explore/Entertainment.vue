1f316f12894c7d1a7379846113af4529949122a0
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-03 19:54:30 +0800

    feat: Added banners

---
+++ src/views/Explore/Entertainment.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="entertainment">
+    <section class="entertainment__banner">
+      <ExploreBanner
+        category="Strategy"
+        timeElapsed="17 minutes ago"
+        title="Genshin Impact: Everything You Need To Know About Fishing"
+        backgroundImageUrl="/fishing.jpg"
+        :isLiveStyle="false"
+      />
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ExploreBanner from "@/components/ExploreBanner.vue";
+export default Vue.extend({
+  components: {
+    ExploreBanner,
+  },
+});
+</script>
---
+++ src/views/Explore/Sports.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="sports">
+    <section class="sports__banner">
+      <ExploreBanner
+        category="Sports"
+        timeElapsed="August 31, 2021"
+        title="Afghan Paralympian beats the odds to compete in Tokyo"
+        backgroundImageUrl="/olympics.webp"
+        :isLiveStyle="false"
+      />
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ExploreBanner from "@/components/ExploreBanner.vue";
+
+export default Vue.extend({
+  components: {
+    ExploreBanner,
+  },
+});
+</script>

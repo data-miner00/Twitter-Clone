1fe6f9f5df2f023c0928d3a4068395294b69d02f
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-03 19:01:43 +0800

    feat: Added explore news banner

---
+++ src/views/Explore/News.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="news">
+    <ExploreBanner
+      category="Economy"
+      timeElapsed="September 2, 2021"
+      title="US university students face back-to-campus vaccine mandates"
+      backgroundImageUrl="/yale.webp"
+      :isLiveStyle="false"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ExploreBanner from "@/components/ExploreBanner.vue";
+export default Vue.extend({
+  components: {
+    ExploreBanner,
+  },
+});
+</script>

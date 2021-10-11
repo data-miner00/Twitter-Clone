55fb1d0e7a3bdf05c360921e99841498cdfcd1b4
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-11 21:33:23 +0800

    feat: PostItem support actions

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,7 +1,19 @@
 <template>
   <article class="post">
     <div class="post__container">
-      <div class="post__container__action-done"></div>
+      <div class="post__container__action-done">
+        <div class="post__container__action-done__icon">
+          <svg viewBox="0 0 24 24" aria-hidden="true">
+            <g>
+              <path
+                fill="currentColor"
+                d="M23.615 15.477c-.47-.47-1.23-.47-1.697 0l-1.326 1.326V7.4c0-2.178-1.772-3.95-3.95-3.95h-5.2c-.663 0-1.2.538-1.2 1.2s.537 1.2 1.2 1.2h5.2c.854 0 1.55.695 1.55 1.55v9.403l-1.326-1.326c-.47-.47-1.23-.47-1.697 0s-.47 1.23 0 1.697l3.374 3.375c.234.233.542.35.85.35s.613-.116.848-.35l3.375-3.376c.467-.47.467-1.23-.002-1.697zM12.562 18.5h-5.2c-.854 0-1.55-.695-1.55-1.55V7.547l1.326 1.326c.234.235.542.352.848.352s.614-.117.85-.352c.468-.47.468-1.23 0-1.697L5.46 3.8c-.47-.468-1.23-.468-1.697 0L.388 7.177c-.47.47-.47 1.23 0 1.697s1.23.47 1.697 0L3.41 7.547v9.403c0 2.178 1.773 3.95 3.95 3.95h5.2c.664 0 1.2-.538 1.2-1.2s-.535-1.2-1.198-1.2z"
+              ></path>
+            </g>
+          </svg>
+        </div>
+        <div class="post__container__action-done__text">You retweeted</div>
+      </div>
       <div class="post__container__content">
         <div class="post__container__content__left">
           <div class="post__container__content__left__avatar">
@@ -229,6 +241,21 @@ export default Vue.extend({
   &__container
     &__action-done
       padding-top: 12px
+      color: $post-action-color
+      font-size: 13px
+      margin-bottom: 4px
+      display: flex
+      &__icon
+        flex-basis: 48px
+        margin-right: 12px
+        display: flex
+        justify-content: flex-end
+        svg
+          display: block
+          height: 15px
+      &__text
+        font-weight: 700
+        line-height: 16px
     &__content
       display: flex
       &__left

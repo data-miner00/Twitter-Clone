5fc116abfb5372fba2aeebef989629a844b2e5b4
data-miner00 <data-miner00@users.noreply.github.com>
2021-11-05 18:27:17 +0800

    feat: Updated noti-item

--- src/views/Notifications/All.vue
+++ src/views/Notifications/All.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="all">
-    <NotiItem />
+    <NotiItem
+      type="follow"
+      username="The world"
+      avatar="/default_profile_bigger.png"
+    />
+    <NotiItem type="like" username="SNH" avatar="/default_profile_bigger.png">
+      <span
+        >Thanks for bringing out this, learned a lot from you. I am glad that
+        Singapore has been sacked or else they will be involved in this
+        predicament that we are in now. Good for them!</span
+      >
+    </NotiItem>
     <NotiSystemItem />
+    <NotiItem
+      type="retweet"
+      username="𝘚𝘭ã𝘳𝘵𝘪𝘣𝘢𝘳𝘵𝘧å𝘴𝘵"
+      avatar="/default_profile_bigger.png"
+    >
+      <span>
+        Regression analysis cheat sheet #DataAnalytics #MachineLearning #BigData
+        #DataScience #IIoT #PyTorch #Python #RStats #TensorFlow #JavaScript
+        #ReactJS #GoLang #CloudComputing #DataScientist #100DaysOfCode
+        #Programming #Coding #flutter #opensource #DevOps #CyberSecurity
+        @Sheraj99 https://pic.twitter.com/elc44X2VeR
+      </span>
+    </NotiItem>
   </div>
 </template>
 

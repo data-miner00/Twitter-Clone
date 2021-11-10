940f011440660ff083f8c5512454ef845f7183c8
data-miner00 <data-miner00@users.noreply.github.com>
2021-11-11 01:51:26 +0800

    feat: Not interested topic page

--- src/views/Topics/NotInterested.vue
+++ src/views/Topics/NotInterested.vue
@@ -1,3 +1,24 @@
 <template>
-  <div class="not-interested">Not Intersted</div>
+  <div class="not-interested">
+    <div class="wrapper">
+      <div class="wrapper-inner">
+        <span>Nothing to see here — yet</span>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="sass" scoped>
+.not-interested
+  .wrapper
+    max-width: calc(400px)
+    padding: 0 32px
+    margin: 32px auto
+    width: 100%
+    &-inner
+      font-weight: 800
+      line-height: 36px
+      margin-bottom: 8px
+      font-size: 31px
+      overflow-wrap: break-word
+</style>

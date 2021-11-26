a39762661a989e8c160a4591cd11c5df159816ed
data-miner00 <data-miner00@users.noreply.github.com>
2021-11-26 22:04:28 +0800

    style: Fix indentation

--- src/components/FollowButton.vue
+++ src/components/FollowButton.vue
@@ -8,18 +8,18 @@
 
 <style lang="sass" scoped>
 .follow-btn
-    min-height: 32px
-    min-width: 79px
-    border-radius: 250px
-    border: 1px solid rgb(121, 75, 196)
-    color: rgb(121, 75, 196)
-    display: flex
-    justify-content: center
-    align-items: center
-    font-weight: 700
-    transition: background .2s
-    &:hover
-        background: rgba(121, 75, 196, .1)
-    div
-        width: fit-content
+  min-height: 32px
+  min-width: 79px
+  border-radius: 250px
+  border: 1px solid rgb(121, 75, 196)
+  color: rgb(121, 75, 196)
+  display: flex
+  justify-content: center
+  align-items: center
+  font-weight: 700
+  transition: background .2s
+  &:hover
+    background: rgba(121, 75, 196, .1)
+  div
+    width: fit-content
 </style>

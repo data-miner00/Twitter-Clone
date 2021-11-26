2124fc1e91cf8a1fc73615bb4b29023f2b50963f
data-miner00 <data-miner00@users.noreply.github.com>
2021-11-26 22:05:14 +0800

    feat: Added bugged following button

---
+++ src/components/FollowingButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    class="following-btn"
+    @mouseover="mouseover"
+    @mouseout="mouseout"
+    :class="{ hover: hovering }"
+  >
+    <div>
+      <span>{{ label }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    hovering: false,
+  }),
+  methods: {
+    mouseover(): void {
+      this.hovering = true;
+    },
+    mouseout(): void {
+      this.hovering = false;
+    },
+  },
+  computed: {
+    label(): string {
+      return this.hovering ? "Unfollow" : "Following";
+    },
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+@import "../assets/sass/_variables"
+
+.following-btn
+  min-height: 32px
+  min-width: 79px
+  border-radius: 250px
+  border: 1px solid rgb(121, 75, 196)
+  color: rgb(121, 75, 196)
+  display: flex
+  justify-content: center
+  align-items: center
+  font-weight: 700
+  transition: background .2s
+  &:hover
+    background: rgba(121, 75, 196, .1)
+  div
+    width: fit-content
+  &.hover
+    div
+      color: $accent-red
+</style>
